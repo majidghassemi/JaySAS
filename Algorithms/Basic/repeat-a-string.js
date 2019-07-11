6fc7b99a2a451efa986abc366ecb43b34e8440ed
Thewn <39520326+Thewm@users.noreply.github.com>
2019-07-11 03:50:54 -0700

    Return repeated string => with 3 approaches

---
+++ Algorithms/Basic/repeat-a-string.js
@@ -0,0 +1,23 @@
+// Return repeated string
+
+// First Approach - Basic
+const repeatString1 = (str, num) => {
+    if (num <= 0) {
+        return str;
+    } else {
+        return str.repeat(num);
+    }
+};
+
+// Second Approach - Intermed
+const repeatString3 = (str, num) => num > 0 ? str.repeat(num) : str;
+
+
+// Second Approach - Advnced => Recursive
+const repeatString2 = (str, num) => {
+    if (num < 0) return '';
+    if (num === 1) return str;
+    else {
+        return str + repeatString2(str, num - 1);
+    }
+};
