3200d0b897a62d2317fceeafd12d3948dc94c53e
Thewn <39520326+Thewm@users.noreply.github.com>
2020-09-06 02:01:42 -0700

    slasher flick solution added

--- Algorithms/Basic/slasher-flick.js
+++ Algorithms/Basic/slasher-flick.js
@@ -1,11 +1,10 @@
 // We need only the remaining part of an array
 // For example: slasherFlick([1, 2, 3], 2); must return [3].
 
-
 // Basic Solutin -- Array.prototype.splice
 const slasherFlick = (arr, index) => {
-    arr.splice(0, index);
-    return arr;
-}
+  arr.splice(0, index);
+  return arr;
+};
 
 const slasherFlick = (arr, index) => arr.slice(index);
