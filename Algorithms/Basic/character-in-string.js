8edc12be24d0a16a72acc96e84d45a078ed9db72
Thewn <39520326+Thewm@users.noreply.github.com>
2020-09-06 04:19:40 -0700

    Count the occurrence of a given character in a string

---
+++ Algorithms/Basic/character-in-string.js
@@ -0,0 +1,20 @@
+// Count the occurrence of a given character in a string?
+
+const countCharacter = (str, substr) => {
+    let counter = 0;
+    for (let i=0; i < str.length; i++) {
+        if (str[i] == substr) counter++;
+        continue;
+    }
+    return counter;
+}
+
+const countCharacter = (str, substr) => {
+  let counter = 0;
+  for (item of str) {
+    if (item == substr) counter++;
+  }
+
+  return counter;
+};
+
