7fb164f7620e0bc6f0c2ef2e520bb65bb572f813
Thewn <39520326+Thewm@users.noreply.github.com>
2020-09-08 02:12:12 -0700

    merge sort

---
+++ Algorithms/Intermed/sort-algorithms/merge-sort.js
@@ -0,0 +1,26 @@
+const mergeSort = (arr) => {
+  if (arr.length < 2) return arr;
+  let middle = Math.floor(arr.length / 2);
+  let left = arr.slice(0, middle);
+  let right = arr.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+function merge(left, right) {
+  let resultArray = [],
+    leftIndex = 0,
+    rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return resultArray
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex));
+}
+// O(nlogn)
