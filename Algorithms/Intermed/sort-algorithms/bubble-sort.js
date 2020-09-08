1ec5fddccc0dc46bd954d161f7ac863f00145e6c
Thewn <39520326+Thewm@users.noreply.github.com>
2020-09-08 00:23:52 -0700

    bubble sort

--- Algorithms/Intermed/sort-algorithms/bubble-sort.js
+++ Algorithms/Intermed/sort-algorithms/bubble-sort.js
@@ -1,7 +1,6 @@
 const bubbleSort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - i - 1; j++) {
-      console.log(arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
@@ -12,4 +11,4 @@ const bubbleSort = (arr) => {
   return arr;
 };
 
-console.log(bubbleSort([2, 1, 0, 5]));
+// O(n^2)
