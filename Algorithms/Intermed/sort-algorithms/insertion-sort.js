929b387b729561c2d642ba6e2e37d37b80aabc64
Thewn <39520326+Thewm@users.noreply.github.com>
2020-09-09 04:54:20 -0700

    insertion sort added

---
+++ Algorithms/Intermed/sort-algorithms/insertion-sort.js
@@ -0,0 +1,13 @@
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = i - 1;
+    }
+    key = arr[j + 1];
+  }
+  return arr;
+};
+
