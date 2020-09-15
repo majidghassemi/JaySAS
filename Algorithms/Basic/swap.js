4b1e5224fc6baab9bdf3d79f27ba50dadb6751dd
Thewn <39520326+Thewm@users.noreply.github.com>
2020-09-15 13:30:23 -0700

    Swap without using 3rd variable

---
+++ Algorithms/Basic/swap.js
@@ -0,0 +1,10 @@
+// Swap without third variable
+
+const swap = (a, b) => {
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    return `After swapping: First number is ${a} and second is ${b}`
+}
+
+console.log(swap(3,5));
