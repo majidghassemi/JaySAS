ce3bd1597965a488a40ef4626cb72566b5e9005f
Thewn <39520326+Thewm@users.noreply.github.com>
2020-09-27 11:20:57 -0700

    Hash tables in js

---
+++ Data Structures/Hash Tables/allAboutHash.js
@@ -0,0 +1,52 @@
+// Insertion, Deletion, Lookup and search's Big O is O(1)
+// When collision (We have 2 or more keys points to one value) occured, It slow down to O(n)
+
+// Implement Hash Table
+
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) { // Setter
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key) { // Getter
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+
+    return undefined;
+  }
+
+  keys() { // Get all of keys in array
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keysArray.push(this.data[i][0][0]);
+      }
+    }
+
+    return keysArray;
+  }
+}
