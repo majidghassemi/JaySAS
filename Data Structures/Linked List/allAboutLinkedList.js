a8e235e32e29bcee14b48784c43395d8f8baa9f8
Majid Ghasemi <mjghcr@gmail.com>
2020-09-28 09:00:54 -0700

    Unnecessary line of code deleted

--- Data Structures/Linked List/allAboutLinkedList.js
+++ Data Structures/Linked List/allAboutLinkedList.js
@@ -4,7 +4,6 @@
 // Each node has value and address of the next node in a list
 // first node is "Head" and last node is "Tail"
 // Lookup for lists' Big O is O(n) -- for both of value and index
-// Lookup for lists' Big O is O(n) -- for both of value and index
 // Read [https://codeburst.io/linked-lists-in-javascript-es6-code-part-1-6dd349c3dcc3]
 
 // Insert to beginning is O(1), to middle is O(n) , to end is O(1)
