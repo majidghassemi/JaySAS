c1d5226e0cb7757f9ff6c8a7942b7e0f03b29204
Majid Ghasemi <mjghcr@gmail.com>
2020-10-01 07:51:50 -0700

    BST implemented in js using linked list

---
+++ Data-Structures/Trees/binary-search-tree/binarySearchTree.js
@@ -0,0 +1,59 @@
+// The left subtree's values is less than root node and right of it is greater
+// For Balanced Tree : Insert, Lookup and Remove's Big O is O(log N)
+// For Unbalanced Tree : Insert, Lookup and Remove's Big O is O(N)
+
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (1) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  lookup(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      return null;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else {
+        return currentNode;
+      }
+    }
+
+    return null;
+  }
+}
