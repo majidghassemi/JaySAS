ebc109affa800d7b034106c7755614cca4c73a2c
Majid Ghasemi <mjghcr@gmail.com>
2020-12-07 14:10:50 +0330

    Fibonacci in iterative way implementation added

--- Algorithms/Basic/fibonacci.js
+++ Algorithms/Basic/fibonacci.js
@@ -1,6 +1,17 @@
-// We implement Fibonacci in recursive way
+// Fibo in iterative way - Good time complexity O(n)
+function iterativeFibo(n) {
+  let helper = [0, 1];
 
-function fibo(n) {
+  for (let i = 2; i <= n; i++) {
+    helper.push(helper[i - 1] + helper[i - 2]);
+  }
+
+  return helper[n];
+}
+
+
+// Fibonacci in recursive way - That has a bad time complexity
+function recursiveFibo(n) {
   if (n < 2) return n;
   return fibo(n - 1) + fibo(n - 2);
 }
