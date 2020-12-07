4e887527b519f62caeebe5c60113d016edbdd8fb
Majid Ghasemi <mjghcr@gmail.com>
2020-12-07 14:44:46 +0330

    Bubble sort implementation changed - make more readable

--- Algorithms/Intermed/sort-algorithms/bubble-sort.js
+++ Algorithms/Intermed/sort-algorithms/bubble-sort.js
@@ -1,6 +1,27 @@
+// Naive approach
+
+// Test array : [100, 93, 1, 0, 5, 9, 34, 29, 25, 54, 49]
+
+const bubbleSort = (arr) => {
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+};
+
+// Better approach
+
 const bubbleSort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < (arr.length - i - 1); j++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
@@ -11,4 +32,8 @@ const bubbleSort = (arr) => {
   return arr;
 };
 
-// O(n^2)
+// If we add some limitation to inner for - time complexity in average case will be better - something about O(n)
+
+
+
+// Time complexity: O(n^2)
