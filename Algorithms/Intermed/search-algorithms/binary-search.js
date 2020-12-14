7f9f07007421bd9d4278c0bb30be6b6a0452d0ae
Majid Ghasemi <mjghcr@gmail.com>
2020-12-14 20:34:20 +0330

    Recursive approach for binary search added

--- Algorithms/Intermed/search-algorithms/binary-search.js
+++ Algorithms/Intermed/search-algorithms/binary-search.js
@@ -1,7 +1,8 @@
-// Binary search - the list should be already sorted - Time complexity O(log n)
+// Binary search - the list should be already sorted - Time complexity in both iterative and recursive is O(log n)
 
 // Test array: [0, 4, 5, 19, 34, 56, 71, 89, 99, 101, 234];
 
+// Iterative method
 const binarySearch = (arr, target) => {
   let low = 0,
     high = arr.length - 1;
@@ -16,3 +17,27 @@ const binarySearch = (arr, target) => {
     }
   }
 };
+
+// Recursive method
+
+const recursiveBinarySearch = (arr, low, high, target) => {
+  if (low === high) {
+    if (low === arr[target]) {
+      return low;
+    } else {
+      return false;
+    }
+  } else {
+    let mid = Math.floor((low + high) / 2);
+    if (target === arr[mid]) {
+      return mid;
+    }
+    if (target < arr[mid]) {
+      return recursiveBinarySearch(arr, low, mid - 1, target);
+    }
+    if (target > arr[mid]) {
+      return recursiveBinarySearch(arr, mid + 1, high, target);
+    }
+  }
+};
+
